de5092edccd9bc9e44bcad309acf8cd456fdf348
Raphael Eduardo <raphaeleduardosc@gmail.com>
2023-03-11 13:53:23 -0300

    Consulta simples com @Query

--- src/main/java/com/example/workshopmongo/repositories/PostRepository.java
+++ src/main/java/com/example/workshopmongo/repositories/PostRepository.java
@@ -3,6 +3,7 @@ package com.example.workshopmongo.repositories;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.workshopmongo.domain.Post;
@@ -10,7 +11,17 @@ import com.example.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 	
-	// Busca posts contendo um dado string no título
+	/* 
+	 * Modelo de consulta alternativa (https://www.mongodb.com/docs/manual/reference/operator/query/regex/)
+	 *  'title' (campo pelo qual quer efetuar a busca).
+	 *  ?0 (primeiro parametro que vem no metodo).
+	 *  'i' (ignora o case sensitive).
+	 */
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> SearchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text); 
-	// Igonore Case para consulta nao ser case sensitive
+	
+	
+	
 }
